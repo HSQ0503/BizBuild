6581a67cfaa4c95473065e324a723cc936991d1a
HSQ0503 <hsq0503@gmail.com>
2025-02-12 20:16:54 -0500

    banana

--- bizbuild/next.config.mjs
+++ bizbuild/next.config.mjs
@@ -1,10 +1,45 @@
+import rehypeShiki from '@leafac/rehype-shiki'
+import nextMDX from '@next/mdx'
+import { Parser } from 'acorn'
+import jsx from 'acorn-jsx'
+import escapeStringRegexp from 'escape-string-regexp'
+import * as path from 'path'
+import { recmaImportImages } from 'recma-import-images'
+import remarkGfm from 'remark-gfm'
+import { remarkRehypeWrap } from 'remark-rehype-wrap'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import shiki from 'shiki'
+import { unifiedConditional } from 'unified-conditional'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+}
+
+function remarkMDXLayout(source, metaName) {
+  let parser = Parser.extend(jsx())
+  let parseOptions = { ecmaVersion: 'latest', sourceType: 'module' }
+
+  return (tree) => {
+    let imp = `import _Layout from '${source}'`
+    let exp = `export default function Layout(props) {
+      return <_Layout {...props} ${metaName}={${metaName}} />
+    }`
+
+    tree.children.push(
+      {
+        type: 'mdxjsEsm',
+        value: imp,
+        data: { estree: parser.parse(imp, parseOptions) },
+      },
+      {
+        type: 'mdxjsEsm',
+        value: exp,
+        data: { estree: parser.parse(exp, parseOptions) },
+      },
+    )
+  }
+}
 
 export default async function config() {
   let highlighter = await shiki.getHighlighter({
